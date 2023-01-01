4a48a4d99951c4d19e7bb08c2d4db9b36545be55
OmbeniFaraja <mwenzangu.ombeni@strathmore.edu>
2023-01-01 20:43:53 +0300

    name display by session

--- resources/views/lecturer/lecturerHome.blade.php
+++ resources/views/lecturer/lecturerHome.blade.php
@@ -8,7 +8,7 @@
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="#">Lecturer</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">{{Auth::user()->name}}</a></li>
                     <li class="nav-item"><a class="nav-link" href="#services">Highlights</a></li>
                     <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
                     <li class="nav-item"><a onclick="event.preventDefault();
